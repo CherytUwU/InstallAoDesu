588634d2c7457e4a740e0f65b1ca3f3992439b86
CherytUwU <79613492+CherytUwU@users.noreply.github.com>
2021-06-27 20:03:24 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 function menuHeaderMostrar(){
-    document.getElementById('menuH').style.width="240px";
+    document.getElementById('menuH').style.right="0px";
+    document.getElementById('menuH').style.visibility="visible";
     document.getElementById('btn1').style.display="none";
     document.getElementById('btn2').style.display="inline";
     document.getElementById('menuHBackground').style.visibility="visible";
 }
 
 function menuHeaderOcultar(){
-    document.getElementById('menuH').style.width="0";
+    document.getElementById('menuH').style.right="-240px";
+    document.getElementById('menuH').style.visibility="hidden";
     document.getElementById('btn1').style.display="inline";
     document.getElementById('btn2').style.display="none";
     document.getElementById('menuHBackground').style.visibility="hidden";
