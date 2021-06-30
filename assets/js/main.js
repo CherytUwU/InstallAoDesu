ee4cbd25e7a868f63c1aefce54a3f74a48e20c1f
CherytUwU <79613492+CherytUwU@users.noreply.github.com>
2021-06-29 21:30:10 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,75 @@ function menuHeaderOcultar(){
     document.getElementById('btn2').style.display="none";
     document.getElementById('menuHBackground').style.visibility="hidden";
 }
+
+// 245
+
+
+function seeAll(){
+    document.getElementById('pcs').style.display="inline-flex"
+    document.getElementById('mobiles').style.display="inline-flex"
+
+    document.getElementById('windows').style.display="flex";
+    document.getElementById('linux').style.display="flex";
+    document.getElementById('mac').style.display="flex";
+
+    document.getElementById('android').style.display="flex";
+    document.getElementById('ios').style.display="flex";
+
+    document.getElementById('downloadall').style.display="none";
+    document.getElementById('menuCatDownload').classList.remove('menuAlto');
+}
+
+function seeWindows(){
+    document.getElementById('pcs').style.display="inline-flex";
+    document.getElementById('mobiles').style.display="none";
+    document.getElementById('linux').style.display="none";
+    document.getElementById('mac').style.display="none";
+    document.getElementById('downloadall').style.display="block";
+
+    document.getElementById('windows').style.display="flex";
+    document.getElementById('menuCatDownload').classList.add('menuAlto');
+    
+}
+
+function seeMac(){
+    document.getElementById('pcs').style.display="inline-flex";
+    document.getElementById('mobiles').style.display="none";
+    document.getElementById('linux').style.display="none";
+    document.getElementById('windows').style.display="none";
+    document.getElementById('downloadall').style.display="block";
+
+    document.getElementById('mac').style.display="flex";
+    document.getElementById('menuCatDownload').classList.add('menuAlto');
+}
+
+function seeLinux(){
+    document.getElementById('pcs').style.display="inline-flex";
+    document.getElementById('mobiles').style.display="none";
+    document.getElementById('mac').style.display="none";
+    document.getElementById('windows').style.display="none";
+    document.getElementById('downloadall').style.display="block";
+
+    document.getElementById('linux').style.display="flex";
+    document.getElementById('menuCatDownload').classList.add('menuAlto')
+}
+
+function seeIos(){
+    document.getElementById('mobiles').style.display="inline-flex";
+    document.getElementById('pcs').style.display="none";
+    document.getElementById('android').style.display="none";
+    document.getElementById('downloadall').style.display="block";
+
+    document.getElementById('ios').style.display="flex"
+    document.getElementById('menuCatDownload').classList.add('menuAlto')
+}
+
+function seeAndroid(){
+    document.getElementById('mobiles').style.display="inline-flex";
+    document.getElementById('pcs').style.display="none";
+    document.getElementById('ios').style.display="none";
+    document.getElementById('downloadall').style.display="block";
+
+    document.getElementById('android').style.display="flex"
+    document.getElementById('menuCatDownload').classList.add('menuAlto')
+}
